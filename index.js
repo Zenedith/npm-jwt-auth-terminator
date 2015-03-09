eb3193aad5ce656e09af5f85fc72f719348fec00
Zenedith <github@zenedith.mailgun.org>
2015-03-09 23:24:42 +0100

    Fixed error response

--- index.js
+++ index.js
@@ -9,35 +9,47 @@ var exports = {};
 var app = server.createServer(options);
 app.on('BadRequestError', function (res, err) {
 
-  var response = new ResponseError('BadRequestError', err.message, 400, '');
+  var response = new ResponseError('BadRequestError', err.message, 400, 'Przekazane dane są niepoprawne lub niepełne');
 
-  res.writeHead(400, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify(response));
+  var data = JSON.stringify(response);
+  res.writeHead(400, {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length
+  });
+  res.end(data);
 
   return (true);
 });
 
 app.on('UnauthorizedError', function (res, err) {
-  var response = new ResponseError('UnauthorizedError', err.message, 401, '');
+  var response = new ResponseError('UnauthorizedError', err.message, 401, 'Aplikacja wymaga autoryzacji');
 
-  res.writeHead(401, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify(response));
+  var data = JSON.stringify(response);
+  res.writeHead(401, {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length
+  });
+  res.end(data);
 
   return (true);
 });
 
-app.on('uncaughtException', function(req, res, err) {
+app.on('uncaughtException', function (req, res, err) {
   logger.warn('uncaught application exception', err);
 
-  var response = new ResponseError('InternalServerError', 'Internal server error', 500, '');
+  var response = new ResponseError('InternalServerError', 'Internal server error', 500, 'Serwis jest chwilowo niedostępny  - spróbuj ponownie za chwilę');
 
-  res.writeHead(500, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify(response));
+  var data = JSON.stringify(response);
+  res.writeHead(500, {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length
+  });
+  res.end(data);
 
   return (true);
 });
 
-process.on('uncaughtException', function(err) {
+process.on('uncaughtException', function (err) {
   logger.error('uncaught PROCESS exception: ', err);
 });
 
